58617cb059ba45829c6e426e6ba663f8c88df692
Geshan Manandhar <geshan@gmail.com>
2016-08-23 12:11:21 +0200

    Minor fix

--- scripts/laracon.js
+++ scripts/laracon.js
@@ -1,5 +1,5 @@
 //  Description:
-//   Hubot - Larabot get tweet aboout Laracon EU
+//   Hubot - Larabot get tweet about Laracon EU
 //
 // Commands:
 //   hubot tell me about laracon eu - returns a random latest tweet about laracon eu
@@ -11,16 +11,15 @@ var request = require('superagent');
 require('superagent-jsonapify')(request);
 
 function fetchRandomTweet(msg)
-{    
+{
     request
     .get('https://extraction.import.io/query/extractor/39ec04b7-c3ef-4025-8510-a27b6ae7093d?_apikey=89ed8eed276e43f7b2c3c3912e77970dbec9d38a7da7c6237e6a60c73d28e3fde90ba4eae62a288874d47608a4ac7a2a7f6119f62d1d284a62ac9dde1789030939ada41027182b6228fb5eff5728f6a0&url=https%3A%2F%2Ftwitter.com%2Fsearch%3Fq%3D%2523LaraconEU')
-    .then(function(res){     
+    .then(function(res){
         var tweets = res.body.extractorData.data[0].group;
         var min = 0;
         var max = 38;
         var randomNumber = Math.floor(Math.random() * (max - min) + min);
         var tweet = tweets[randomNumber];
-        
 
         msg.send('@' + msg.message.user.name + ' ' + tweet.author_name[0].text + '(@' + tweet.author_handle[0].text + ')' + ' tweeted "' + tweet.tweet[0].text + '"');
     }).catch(function(e){
